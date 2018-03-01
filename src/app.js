7a2c3299ad026d66cff75c9683f265ca976c8034
hdn <hieudn5293@yahoo.de>
2018-03-01 22:08:00 +0100

    inline_styles.js successfully ported to aurelia

--- src/app.js
+++ src/app.js
@@ -1,95 +1,19 @@
 
-import {Todo} from './todo'
 import {contextMenu} from 'jquery-contextmenu'
-import playNew from './playground/playNew'
-import play from "./playground/play"
-import porterplay from "./playground/porterplay"
-
+let inSty = require('./app_logic/inline_styles')
 
 export class App {
     constructor() {
-        this.heading = "Todos"
-        this.todos = []
-        this.todoDescription = ''
     }
 
     multipleCalls() {
-        this.launchCM()
-        // this.inlineStyling()
-        // let todotest = new Todo("hi")
-        // todotest.logger()
-        play.hello()
-        porterplay.sayHi()
-
-        // requirejs(['./playground/play','jquery-contextmenu'], function (play,cm) {
-        //     console.log("from require")
-        //     console.log(play)
-        //     $(function () {
-        //         $.contextMenu({
-        //             selector: '#container',
-        //             // trigger: 'left',
-        //             // delay:1,
-        //             items: {
-        //                 SplitDiv: {
-        //                     name: "Split Div From require",
-        //                 },
-
-        //                 Sep: "--------------------------",
-        //             },
-        //         });
-        //     });
-        // })
-    }
-
-    addTodo() {
-        if(this.todoDescription) {
-            this.todos.push(new Todo(this.todoDescription))
-            this.todoDescription = ''
-        }
-    }
-
-    removeTodo(todo) {
-        let index = this.todos.indexOf(todo)
-        if(index !== -1) {
-            this.todos.splice(index,1)
-        }
-    }
-
-    clickTest() {
-        console.log("success")
+        console.log(inSty)
         let container = document.getElementById("container")
-        console.log(container)
+        let box = {
+            border: "3px dotted blue"
+        }
+        inSty.addInlineStyle(container,box)
     }
-
-    launchCM() {
-        $(function () {
-            $.contextMenu({
-                selector: '#container',
-                // trigger: 'left',
-                // delay:1,
-                items: {
-                    SplitDiv: {
-                        name: "Split Div From require",
-                        callback: play.hello
-                    },
-
-                    Sep: "--------------------------",
-                },
-            });
-        });
-
-    }
-
-    // inlineStyling() {
-    //     let is = new Play("hi")
-    //     let container = document.getElementById("container")
-        
-    //     let box = { "border": "3px dotted blue" }
-    //     console.log(is)
-    //     is.addInlineStyle(container, box)
-    // }
-
-
 
 }
 
